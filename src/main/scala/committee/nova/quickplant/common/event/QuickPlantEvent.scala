bb178f9043c26e107509b19d726190e4591cc6d4
MikhailTapio <81358657+MikhailTapio@users.noreply.github.com>
2022-04-14 00:12:02 +0800

    Non-single stack

--- src/main/scala/committee/nova/quickplant/common/event/QuickPlantEvent.scala
+++ src/main/scala/committee/nova/quickplant/common/event/QuickPlantEvent.scala
@@ -3,13 +3,17 @@ package committee.nova.quickplant.common.event
 import committee.nova.quickplant.common.config.CommonConfig
 import committee.nova.quickplant.common.util.Utilities._
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
-import net.minecraftforge.event.entity.item.{ItemExpireEvent, ItemTossEvent}
+import net.minecraft.entity.item.EntityItem
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
+import net.minecraftforge.event.entity.item.ItemExpireEvent
 
 class QuickPlantEvent {
   @SubscribeEvent
-  def onItemToss(event: ItemTossEvent): Unit = {
+  def onItemDrop(event: EntityJoinWorldEvent): Unit = {
     if (event.isCanceled) return
-    val itemEntity = event.entityItem
+    val entity = event.entity
+    if (!entity.isInstanceOf[EntityItem]) return
+    val itemEntity = entity.asInstanceOf[EntityItem]
     if (isPlant(itemEntity.getEntityItem) == 0) return
     itemEntity.lifespan = CommonConfig.refreshInterval
   }
@@ -18,10 +22,19 @@ class QuickPlantEvent {
   def onItemExpire(event: ItemExpireEvent): Unit = {
     if (event.isCanceled) return
     val itemEntity = event.entityItem
-    val plantType = isPlant(itemEntity.getEntityItem)
+    val stack = itemEntity.getEntityItem
+    val plantType = isPlant(stack)
     if (plantType == 0) return
     if (itemEntity.age >= CommonConfig.expireTime) return
-    if (tryPlantThere(itemEntity, isSeed = plantType == 1)) return
+    if (!tryPlantThere(itemEntity, isSeed = plantType == 1)) {
+      event.extraLife = CommonConfig.refreshInterval
+      event.setCanceled(true)
+      return
+    }
+    val newStack = stack.copy()
+    newStack.stackSize -= 1
+    if (newStack.stackSize <= 0) return
+    itemEntity.setEntityItemStack(newStack)
     event.extraLife = CommonConfig.refreshInterval
     event.setCanceled(true)
   }
