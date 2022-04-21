7fb75c265d972a30d2678f175e56039c30983724
MikhailTapio <mikhailtapio13468@163.com>
2022-04-21 14:13:34 +0800

    Server Compatibility

--- src/main/scala/committee/nova/quickplant/common/event/QuickPlantEvent.scala
+++ src/main/scala/committee/nova/quickplant/common/event/QuickPlantEvent.scala
@@ -21,7 +21,7 @@ class QuickPlantEvent {
     val stack = itemEntity.getEntityItem
     val plantType = isPlant(stack)
     if (plantType == 0) return
-    if (itemEntity.getAge >= CommonConfig.expireTime) return
+    if (itemEntity.ticksExisted >= CommonConfig.expireTime) return
     if (!tryPlantThere(itemEntity, isSeed = plantType == 1)) {
       event.extraLife = CommonConfig.refreshInterval
       event.setCanceled(true)
